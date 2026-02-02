271acafccae573810d5e0d10f69a75f470b03278
mitesh4949 <mitesh@ganatrainfo.com>
2026-02-02 15:31:30 +0530

    test changes

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 const Home = () => {
   return (
     <h1 className="text-4xl font-bold text-center text-blue-600 mt-10">
-      This is my first projec next js by Mitesh parmar
+      Next js project start with developer
     </h1>
   );
 };
