226a3e55b598d712d89c3e892bc6aeb2a3646084
mitesh4949 <mitesh@ganatrainfo.com>
2026-02-02 12:37:30 +0530

    text changes by mitesh

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 const Home = () => {
   return (
     <h1 className="text-4xl font-bold text-center text-blue-600 mt-10">
-      Hello world mitesh parmar
+      This is my first projec next js by Mitesh parmar
     </h1>
   );
 };
